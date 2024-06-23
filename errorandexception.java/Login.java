5ff202da4040fc92527ff6908863d50de0d62fc0
Nirmalagithub2408 <nimijackie@gmail.com>
2024-06-24 01:33:22 +0530

    first commit

---
+++ errorandexception.java/Login.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) 
+    {
+    throw Invalid_LoginException extends Exception
+
+       scanner sc = new scanner (System.in);
+       System.out.println("Enter the user name: ");
+        String u = sc.nextLine();
+        System.out.println("Enter the password: ");
+        string p = sc.nextLine();
+    
+    try
+    {
+        if (u.equals (p))
+        {
+            System.out.println("Valid Login");
+        else
+    throw new Invalid_LoginException();
+    
+}
+catch(Invalid_LoginException e)
+{
+    System.out.println("Exception: "+a+" :-Invalid Login!!!");
+}
+    }
+
+}       
+}   
+
